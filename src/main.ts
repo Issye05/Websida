8d1de11f658c860fa65d4ffacfe9388e0bbbfcd8
Isak Edholm <153501059+Issye05@users.noreply.github.com>
2024-05-12 21:06:07 +0200

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
-#!/usr/bin/env node
 import express from "express";
-import mysql from "mysql2/promise";
+import mysql, { RowDataPacket } from "mysql2/promise";
 import crypto from "crypto";
-import { v4 as uuid } from 'uuid';
+import { v4 as uuid } from "uuid";
+import cookie from "cookie-parser";
 
 async function main() {
   const app = express();
 
-  app.use(logger);
+  app.get("/login");
+  app.use(cookie());
 
   const db = await mysql.createConnection({
     host: "localhost",
@@ -16,60 +17,52 @@ async function main() {
     database: "FotbollsLagDB",
   });
 
-  app.get("/LagNamn", async function (req, res) {
-    let question = "SELECT * FROM LagNamn;";
-    console.log(question);
-    const result = await db.execute(question);
-    const data = result.values().next().value;
-    console.log(data);
-    res.send(data);
-  });
+  app.get(
+    "/login",
+    async function (req: express.Request, res: express.Response) {
+      let count = 0;
+      type TokenData = { [key: string]: string };
+      const token_storage: TokenData = {};
 
-  app.get("/input", async function (req, res) {
-    const bmw_modeller = req.query.bmw_modeller;
-    const LagNamn = "";
-    const Ekonomi = 0;
-    // const question = "SELECT * FROM LagNamn;";
-    const question = "INSERT INTO msg VALUES (null, ?, ?, ?);";
-    // console.log(question);
-    const result = await db.execute(question, [LagNamn, Ekonomi]);
-    // const data = result.values().next().value;
-    res.send(bmw_modeller);
-    // res.send("Hello!" + data[0].pris); visa Hello + pris
-  });
+      console.log("Type of user:", typeof req.query.user);
+      console.log("Type of password:", typeof req.query.password);
 
-  app.get("/login", async function (req, res) {
-    if (
-      req.query &&
-      typeof req.query.user === "string" &&
-      typeof req.query.password === "string"
-    ) 
-      const user_name = req.query.user;
-      const password = crypto
-        .createHash("md5")
-        .update(String(req.query.password))
-        .digest("hex");
-      let question = 'SELECT name,password FROM user WHERE name =?';
-      console.log(user_name);
-      console.log(req.query.password);
-      console.log(db.format(question, [user_name]));
-      const user_answer = await db.execute(question, [user_name]);
-      const password_from_database = user_answer.values().next().value[0].password;
-      console.log(password_from_database);
-      if (password_from_database === password) [
-        const token = uuid();
-        console.log(token);
-        token_storage[token] = user_name;
-        res.cookie('login', token, {maxAge: 10000000});
-        res.send('Rätt lösenord');
-        else {
-          res.send('fel lösenord!');
+      const user_name = req.query.user as string;
+
+      if (req.query && typeof req.query.password === "string") {
+        const password = crypto
+          .createHash("md5")
+          .update(String(req.query.password))
+          .digest("hex");
+        let question = "SELECT name,password FROM user WHERE name =?";
+        console.log(user_name);
+        console.log(req.query.password);
+        console.log(db.format(question, [user_name]));
+        const [rows] = await db.execute(question, [user_name]);
+        const user_answer = rows as RowDataPacket[];
+        if (user_answer.length > 0) {
+          const password_from_database = user_answer[0]?.password;
+          console.log(password_from_database);
+          if (password_from_database === password) {
+            const token = uuid();
+            console.log(token);
+            token_storage[token] = user_name;
+            count = Number(req.cookies.count);
+            count++;
+            res.cookie("count", count, { maxAge: 10000000 });
+            res.cookie("login", token, { maxAge: 10000000 });
+            res.send("Rätt lösenord");
+          } else {
+            res.send("fel lösenord!");
+          }
+        } else {
+          res.send("Användaren hittades inte");
         }
-        else {
-          res.send('Du har inte skrivit lössenord eller användarnamn')
-        }
-      ]
-  });
+      } else {
+        res.send("Du har inte skrivit lösenord eller användarnamn");
+      }
+    },
+  );
 
   const port = 8080;
 
